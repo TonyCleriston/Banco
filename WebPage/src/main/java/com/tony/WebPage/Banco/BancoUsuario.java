ff42a4a2701d341b288a3c0d0f7d6034bd6e203d
Tony <114925619+TonyCleriston@users.noreply.github.com>
2022-10-25 17:28:43 -0300

    Delete WebPage directory

--- WebPage/src/main/java/com/tony/WebPage/Banco/BancoUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.tony.WebPage.Banco;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tony.WebPage.entidade.Cadastro;
-
-public interface BancoUsuario  extends CrudRepository< Integer , Cadastro >{
-    
-}
